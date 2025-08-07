0fccf394e8766b93537d700d466cd578b4036b3d
suzuki-aki-rin <aki.suzuki.rin@gmail.com>
2025-08-07 16:24:21 +0900

    refactor: Change print() to logging. Add argument notations

--- api/ws_handler.py
+++ api/ws_handler.py
@@ -1,8 +1,28 @@
 import json
+import logging
 
 from fastapi import WebSocket
+
 from api.translator import Translator
 from config import Translation
+
+
+#  SECTION:=============================================================
+#            Logger
+#  =====================================================================
+
+# Setup main logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+
+#  SECTION:=============================================================
+#            Functions, utility
+#  =====================================================================k
 
 
 def build_message_to_obs(text: str, is_final: bool, language_code: str) -> str:
@@ -15,53 +35,58 @@ def build_message_to_obs(text: str, is_final: bool, language_code: str) -> str:
     return json.dumps(message, ensure_ascii=False)
 
 
-def print_message(message: str, is_final: bool, language_code: str):
+def print_message(message: str, is_final: bool, language_code: str) -> None:
     is_final_text = "[Final  ]" if is_final else "[Interim]"
-
-    print(f"{is_final_text} {language_code}:{message}")
+    logger.info(f"{is_final_text} {language_code}: {message}")
 
 
 def shorten_language_code(language_code: str) -> str:
     return language_code[:2]
 
 
+#  SECTION:=============================================================
+#            Class
+#  =====================================================================
+
+
 class WsMessageProcessor:
+    translator: Translator | None
+
     def __init__(self):
         self.translator = None
 
     async def process_ws_message(
         self,
         ws_message_source: WebSocket,
-        ws_messgae_target: WebSocket,
+        ws_message_target: WebSocket | None,
         message: str,
-    ):
-        """
-        Process the received JSON string from the WebSocket.
-        Prints whether it's an interim or final result and the recognized text itself.
-        """
+    ) -> None:
+        """Main entry point to process a WebSocket message."""
         try:
             # JSON decode
             payload = json.loads(message)
-            text = payload.get("recogText", "")
-            is_final = payload.get("isFinal", False)
-            language = payload.get("language", {})
-            language_code = language.get("code")
-            language_label = language.get("label")
+            text: str = payload.get("recogText", "")
+            is_final: bool = payload.get("isFinal", False)
+            language: dict | None = payload.get("language", {})
+            language_code: str | None = language.get("code") if language else None
+            language_label: str | None = language.get("label") if language else None
 
             # send to OBS
-            if ws_messgae_target:
-                message_for_obs = build_message_to_obs(text, is_final, language_code)
-                await ws_messgae_target.send_text(message_for_obs)
+            if ws_message_target:
+                message_for_obs = build_message_to_obs(
+                    text, is_final, language_code or ""
+                )
+                await ws_message_target.send_text(message_for_obs)
 
             # console output
-            print_message(text, is_final, language_code)
+            print_message(text, is_final, language_code or "")
 
             # Do if recognition text is final
             if is_final:
                 # send to Translator
                 if Translation.ENABLE == "True":
                     if Translation.SOURCE_LANGUAGE != shorten_language_code(
-                        language_code
+                        language_code or ""
                     ):
                         raise ValueError(
                             f"language code mismatch: {Translation.SOURCE_LANGUAGE} != {language_code}"
@@ -72,11 +97,11 @@ class WsMessageProcessor:
                             self.translator = Translator(
                                 Translation.SOURCE_LANGUAGE, Translation.TARGET_LANGUAGE
                             )
-                        json_result = await self.translator.translate(text)
-                        if ws_messgae_target:
-                            await ws_messgae_target.send_text(json_result)
+                        json_result = await self.translator.translate_as_json(text)
+                        if ws_message_target:
+                            await ws_message_target.send_text(json_result)
 
         except json.JSONDecodeError:
-            print(f"Received non-JSON message: {message}")
+            logger.warning(f"Received non-JSON message: {message}")
         except Exception as e:
-            print(f"Error processing WebSocket message: {e}")
+            logger.error(f"Error processing WebSocket message: {e}", exc_info=True)
